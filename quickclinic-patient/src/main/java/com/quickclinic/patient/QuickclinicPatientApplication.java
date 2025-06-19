97c70f82cfb453a6b63bed72f3a7bd7605b451af
aryandev <165481629+aryan735@users.noreply.github.com>
2025-06-20 02:40:13 +0530

    Project Creation Initial Commit

---
+++ quickclinic-patient/src/main/java/com/quickclinic/patient/QuickclinicPatientApplication.java
@@ -0,0 +1,13 @@
+package com.quickclinic.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickclinicPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickclinicPatientApplication.class, args);
+	}
+
+}
---
+++ quickclinic-patient/src/test/java/com/quickclinic/patient/QuickclinicPatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickclinic.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickclinicPatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
