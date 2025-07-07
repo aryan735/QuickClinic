76bf2d363da358d4d695c035cb3f138450814f5b
aryandev <165481629+aryan735@users.noreply.github.com>
2025-07-07 15:28:06 +0530

    Implemented Exception model for global exception handler

--- quickclinic-patient/src/main/java/com/quickclinic/patient/exceptions/ExceptionModel.java
+++ quickclinic-patient/src/main/java/com/quickclinic/patient/exceptions/ExceptionModel.java
@@ -1,4 +1,14 @@
 package com.quickclinic.patient.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class ExceptionModel {
+    private LocalDateTime timestamp;
+    private String message;
+    private String errorCode;
 }
