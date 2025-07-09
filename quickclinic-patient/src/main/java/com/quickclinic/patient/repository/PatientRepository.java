05ed5bced347a0a14a8380e1616ef9ee8caba116
aryandev <165481629+aryan735@users.noreply.github.com>
2025-07-09 16:01:52 +0530

    Fixed the data type of dob in the jpql query of updatePatientDetails method

--- quickclinic-patient/src/main/java/com/quickclinic/patient/repository/PatientRepository.java
+++ quickclinic-patient/src/main/java/com/quickclinic/patient/repository/PatientRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -19,5 +20,5 @@ public interface PatientRepository extends JpaRepository <PatientModel,Long> {
    @Modifying
     @Transactional
     @Query("UPDATE PatientModel p SET p.doctorName= :doctorName, p.doctorId=:doctorId, p.name=:name,p.email=:email,p.dob=:dob,p.age=:age,p.gender=:gender,p.phoneNo=:phoneNo,p.alternativePhoneNo=:altNo,p.address=:address,p.city= :city,p.state= :state,p.zip= :zip WHERE p.patientId = : patientId")
-    int updatePatientDetails(@Param("doctorName") String doctorName, @Param("doctorId") Long doctorId, @Param("name") String name, @Param("email") String email, @Param("dob") String dob,@Param("age") int age, @Param("gender") String gender, @Param("phoneNo") String phoneNo, @Param("altNo") String altNo, @Param("address") String address, @Param("city") String city, @Param("state") String state, @Param("zip") String zip, @Param("patientId") Long patientId);
+    int updatePatientDetails(@Param("doctorName") String doctorName, @Param("doctorId") Long doctorId, @Param("name") String name, @Param("email") String email, @Param("dob") LocalDate dob, @Param("age") int age, @Param("gender") String gender, @Param("phoneNo") String phoneNo, @Param("altNo") String altNo, @Param("address") String address, @Param("city") String city, @Param("state") String state, @Param("zip") String zip, @Param("patientId") Long patientId);
 }
