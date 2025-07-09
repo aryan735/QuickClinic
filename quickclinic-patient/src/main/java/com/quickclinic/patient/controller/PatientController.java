1796dd503282c6cbaea18334db5f0cdfc0b9e21f
aryandev <165481629+aryan735@users.noreply.github.com>
2025-07-09 15:54:36 +0530

    Fixed the parameter of updatePatient method, replace the PatientRequestDto to PatientUpdateDto

--- quickclinic-patient/src/main/java/com/quickclinic/patient/controller/PatientController.java
+++ quickclinic-patient/src/main/java/com/quickclinic/patient/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.quickclinic.patient.controller;
 
 import com.quickclinic.patient.controller.dtos.PatientRequestDto;
 import com.quickclinic.patient.controller.dtos.PatientResponseDto;
+import com.quickclinic.patient.controller.dtos.PatientUpdateDto;
 import com.quickclinic.patient.service.PatientServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,7 +35,7 @@ public class PatientController {
         return ResponseEntity.status(HttpStatus.OK).body(patientById);
     }
     @PutMapping("/update-patient-details/patientId/{id}")
-    public ResponseEntity<String> updatePatient(@RequestBody PatientRequestDto patientRequestDto,@PathVariable Long id){
+    public ResponseEntity<String> updatePatient(@RequestBody PatientUpdateDto patientRequestDto, @PathVariable Long id){
         patientService.updatePatientDetails(patientRequestDto,id);
         return ResponseEntity.status(HttpStatus.OK).body("Patient Application updated successfully!");
     }
