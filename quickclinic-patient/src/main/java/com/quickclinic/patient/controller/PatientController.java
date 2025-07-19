bcf69fe74bd6c58c9475d2886068e25ea4920b31
aryandev <165481629+aryan735@users.noreply.github.com>
2025-07-19 18:17:20 +0530

    Refactored CoustomUserDetails for future role handling

--- quickclinic-patient/src/main/java/com/quickclinic/patient/controller/PatientController.java
+++ quickclinic-patient/src/main/java/com/quickclinic/patient/controller/PatientController.java
@@ -7,9 +7,13 @@ import com.quickclinic.patient.service.PatientServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RestController
 @RequestMapping("/patients")
@@ -29,8 +33,11 @@ public class PatientController {
         return ResponseEntity.status(HttpStatus.OK).body(patientById);
     }
 
-    @GetMapping("/get-All-patients/userId/{id}")
-    public ResponseEntity<List<PatientResponseDto>> getAllPatientsOfAUser(@PathVariable Long id){
+    @GetMapping("/get-All-patients")
+    public ResponseEntity<List<PatientResponseDto>> getAllPatientsOfAUser(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+
         List<PatientResponseDto> patientById = patientService.getAllPatientsOfAUser(id);
         return ResponseEntity.status(HttpStatus.OK).body(patientById);
     }
