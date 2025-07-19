1c0ebbdd417a3a10ebe73b0b6dd8723b5256f130
aryandev <165481629+aryan735@users.noreply.github.com>
2025-07-19 20:27:23 +0530

    Added getter setter and all constructors by annotations

--- quickclinic-patient/src/main/java/com/quickclinic/patient/client/dto/BasicUserInfoDto.java
+++ quickclinic-patient/src/main/java/com/quickclinic/patient/client/dto/BasicUserInfoDto.java
@@ -1,5 +1,12 @@
 package com.quickclinic.patient.client.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BasicUserInfoDto {
     private Long id;
     private String name;
